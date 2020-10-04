f7f9463e87b39eb0b21d22897e15193895d80679
MESSAOUI <mohand-ameziane.messaoui@etu.u-pec.fr>
2020-10-04 09:09:38 +0200

    clean

--- Back-End/README.md
+++
@@ -1,8 +0,0 @@
-# Lyamn-api
-
-
-## Getting Started
-install dependencies using npm 
-```
-npm install
-```
--- Back-End/configurations/responsesTempalte.js
+++
@@ -1,7 +0,0 @@
-module.exports = (res, status = 500, success = false, message = "error", data = null) => {
-    return res.status(status).json({
-        success,
-        message,
-        data: data === null ? [] : data
-    });
-}
